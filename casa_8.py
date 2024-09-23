0bcf6e331cbd56b995476b3c9b9977d11d1e1775
DiegoGA05 <dagaray2@miners.utep.edu>
2024-09-23 13:36:52 -0600

    Added iterrows and apply method to casa

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,18 @@
+import pandas as pd
 
+
+df = pd.read_csv("big-mac-full-index.csv")
+df.head()
+
+## Index Method
+# for index in df.index:
+#     print(df['iso_a3'][index],
+#           df['name'][index])
+
+## Iterrows Method
+for i,r in df.iterrows():
+    print(r['iso_a3'],
+          r['name'])
+
+## Apply Method
+print(df.apply(lambda row: row['iso_a3'], axis = 1))
