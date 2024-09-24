56fd4422cd7572e47385583de8ee164fe1e90434
DiegoGA05 <dagaray2@miners.utep.edu>
2024-09-24 09:09:41 -0600

    Differentiating methods

--- casa_8.py
+++ casa_8.py
@@ -4,15 +4,15 @@ import pandas as pd
 df = pd.read_csv("big-mac-full-index.csv")
 df.head()
 
-## Index Method
+### Index Method
 # for index in df.index:
 #     print(df['iso_a3'][index],
 #           df['name'][index])
 
-## Iterrows Method
+### Iterrows Method
 for i,r in df.iterrows():
     print(r['iso_a3'],
           r['name'])
 
-## Apply Method
+### Apply Method
 print(df.apply(lambda row: row['iso_a3'], axis = 1))
